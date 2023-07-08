ae08803fc843ce99a0cdffe315cb62f8da90cf09
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-07-08 23:43:34 +0300

    edit text

--- src/components/GameOne.tsx
+++ src/components/GameOne.tsx
@@ -6,7 +6,7 @@ function GameOne() {
       <h1 className="fortnite text-5xl text-center lg:text-7xl lg:text-left text-primary">Kilalao mampiasa saina</h1>
       <div className="lg:grid lg:grid-cols-3 space-y-5 lg:space-y-0">
         <div className="flex items-center">
-          <p className="first-letter:text-2xl text-center text-xl lg:text-left lg:first-letter:text-4xl lg:text-2xl">Ce jeux consiste a faire trouver deux images identiques pour gagner</p>
+          <p className="first-letter:text-2xl text-center text-xl lg:text-left lg:first-letter:text-4xl lg:text-2xl">Tiana havoaka daholo ireo sary ambadiky ny efajoro. Tadiavina tsiroaroa izy ireo, izany hoe mila miasa ny saina sy ny fitadidiana ireo toerana misy ireo sary tsirairay</p>
         </div>
         <div className="col-span-2">
           <MemoryGame />
