7b27781e74b42dfe13a1ad9a67363ccb23bbc0b9
muriel <murielarisoaran@gmail.com>
2023-07-08 14:12:19 +0300

    welcome

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,30 +1,11 @@
-import kids from "../assets/Kids.svg";
+
 import triangle3d from "../assets/triangle3D.svg";
 import star3d from "../assets/star3D.svg";
 
 function Presentation() {
   return (
     <div>
-      <div className="grid lg:grid-cols-2 ">
-        <div className="flex flex-col justify-between gap-8 py-44 px-16">
-          <div>
-            <h1 className="fortnite text-8xl font-bold">
-              Tongasoa eto amin'ny TIA ZAZA
-            </h1>
-          </div>
-          <div>
-            <p className="text-2xl">Andeha isika hilalao rankizy !</p>
-          </div>
-          <div className="">
-            <button className="text-2xl bg-[#5658D9] hover:bg-[#353789] hover:transition hover:duration-300 text-base-100 p-4 rounded-full font-bold">
-              Ireo kilalaontsika {"->"}{" "}
-            </button>
-          </div>
-        </div>
-        <div className="">
-          <img src={kids} alt="" className="" />
-        </div>
-      </div>
+
       <div className="grid lg:grid-cols-5 bg-red-200">
         <div>
           <img src={triangle3d} alt="" />
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,4 +1,5 @@
 import logo from '../assets/TiaZaza.svg';
+import kids from "../assets/Kids.svg";
 function Welcome() {
   return (
     <div>
@@ -46,6 +47,27 @@ function Welcome() {
               <li><a><span>🌚</span><label htmlFor="">Maizina</label></a></li>
             </ul>
           </div>
+        </div>
+      </div>
+
+      <div className="grid lg:grid-cols-2 ">
+        <div className="flex flex-col justify-between gap-8 py-44 px-16">
+          <div>
+            <h1 className="fortnite text-8xl font-bold">
+              Tongasoa eto amin'ny TIA ZAZA
+            </h1>
+          </div>
+          <div>
+            <p className="text-2xl">Andeha isika hilalao rankizy !</p>
+          </div>
+          <div className="">
+            <button className="text-2xl bg-[#5658D9] hover:bg-[#353789] hover:transition hover:duration-300 text-base-100 p-4 rounded-full font-bold">
+              Ireo kilalaontsika {"->"}{" "}
+            </button>
+          </div>
+        </div>
+        <div className="">
+          <img src={kids} alt="" className="" />
         </div>
       </div>
     </div>
