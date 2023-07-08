8abd0c911a241c27c70bce3fdd174d58db56ccab
MurielArii <murielarisoaran@gmail.com>
2023-07-08 14:31:00 +0300

    failed game one

--- src/components/GameOne.tsx
+++ src/components/GameOne.tsx
@@ -1,23 +1,7 @@
-import { Canvas } from "@react-three/fiber"
-import { BufferAttribute, BufferGeometry } from "three";
-import { Shapes } from "../3D/Shapes";
-
 function GameOne() {
-  const triangleGeometry = new BufferGeometry();
-  triangleGeometry.setAttribute(
-    "position",
-    new BufferAttribute(new Float32Array([0, 1, 0, -1, -1, 0, 1, -1, 0]), 3)
-  );
   return (
     <div className="px-[10%] space-y-5">
-      <h1>Guess the shape</h1>
-      <div className="h-[75vh] bg-red-300">
-        <Canvas>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Shapes scale={5}/>
-        </Canvas>
-      </div>
+      <h1>Game One</h1>
     </div>
   )
 }
