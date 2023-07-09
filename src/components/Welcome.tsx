c97e58e2131931784b229b5f5b1c9070d141868f
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-07-09 14:12:16 +0300

    almost done

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -93,12 +93,20 @@ function Welcome() {
           <div className=" flex justify-center lg:justify-start">
             <div>
               <h1 className="font-bold lg:text-xl 2xl:text-3xl">Koa Andeha isika <span className="text-secondary lg:text-3xl 2xl:text-4xl">hilalao</span> rankizy !</h1>
-              <div className="flex gap-2 items-center">
+              <div className="hidden sm:flex sm:gap-2 sm:items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                 </svg>
                 <div>
-                  Afaka aodinao le sary amin'ny sisiny io
+                  Afaka ahodinao ilay sary amin'ny sisiny io
+                </div>
+              </div>
+              <div className="flex gap-2 items-center sm:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                </svg>
+                <div>
+                  Afaka ahodinao ilay sary ao ambony
                 </div>
               </div>
             </div>
